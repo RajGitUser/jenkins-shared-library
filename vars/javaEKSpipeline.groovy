70f2b1bda7eb8dbaff9d2b49a59b9b2c2c381170
Rajkumar <rajkumarmanda@outlook.com>
2025-12-31 04:41:59 -0500

    jenkins

--- vars/javaEKSpipeline.groovy
+++ vars/javaEKSpipeline.groovy
@@ -9,7 +9,6 @@ def call (Map configMap){
         }
         environment {
             COURSE = "Jenkins"
-            appVersion = ""
             ACC_ID = "848332098195"
             PROJECT = configMap.get("project")
             COMPONENT = configMap.get("component")
